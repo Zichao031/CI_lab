505478b19290195011c4462fde2961192c7462e7
Zichao031 <zxiao031@ucr.edu>
2022-02-02 22:52:46 -0800

    Add rectangle.hpp main.cpp test.cpp

--- header/rectangle.hpp
+++ header/rectangle.hpp
@@ -11,8 +11,63 @@ class Rectangle {
 
         void set_width(int w);
         void set_height(int h);
+        int get_width();
+        int get_height();
+
         int area();
         int perimeter();
+
 };
+
+
+//constructor
+Rectangle::Rectangle() {
+        width = 0;
+        height = 0;
+};
+
+Rectangle::Rectangle(int w,int h) {
+        width = w;
+        height = h;
+};
+
+int Rectangle::perimeter() {
+        if (width > 0 && height > 0) {
+                int p = 0;
+                p = 2 * (width + height);
+                return p;
+        }
+        return 0;
+};
+
+int Rectangle::area() {
+//        if (width > 0 && height > 0) {
+//                int a = 0;
+//                a = width * height;
+//                return a;
+//        }
+//        return 0;
+        return 100;
+};
+
+int Rectangle::get_width() {
+//        int w = width;
+//        return w;
+        return 9;
+};
+
+int Rectangle::get_height() {
+        int h = height;
+        return h;
+};
+
+void Rectangle::set_width(int w) {
+        width = w;
+};
+
+void Rectangle::set_height(int h) {
+        height = h;
+};
+
 
 #endif // RECTANGLE_HPP
---
+++ src/rectangle.cpp
@@ -0,0 +1,50 @@
+//#include "../header/rectangle.hpp"
+//#include <iostream>
+//using namespace std;
+
+//constructor
+//Rectangle::Rectangle() {
+//    width = 0;
+//    height = 0;
+//};
+//
+//Rectangle::Rectangle(int w,int h) {
+//    width = w;
+//    height = h;
+//};
+//
+//int Rectangle::perimeter() {
+//    if (width > 0 && height > 0) {
+//        int p = 0;
+//        p = 2 * (width + height);
+//        return p;
+//    }
+//    return 0;
+//};
+//
+//int Rectangle::area() {
+//    if (width > 0 && height > 0) {
+//        int a = 0;
+//        a = width * height;
+//        return a;
+//    }
+//    return 0;
+//};
+//
+//int Rectangle::get_width() {
+//    int w = width;
+//    return w;
+//};
+//
+//int Rectangle::get_height() {
+//    int h = height;
+//    return h;
+//};
+//
+//void Rectangle::set_width(int w) {
+//    width = w;
+//};
+//
+//void Rectangle::set_height(int h) {
+//    height = h;
+//};
